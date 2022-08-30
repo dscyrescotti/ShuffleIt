60038013accf3330b6794a5c4fa5b143744611f2
dscyrescotti <dscyrescotti@gmail.com>
2022-08-30 22:34:01 +0630

    feat: link swift package to demo

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "ShuffleIt",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
-        .tvOS(.v14),
-        .watchOS(.v7)
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(
