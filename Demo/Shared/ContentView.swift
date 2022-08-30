e3ce88110c4d2d28da282e5f2c38d225eb1b3750
dscyrescotti <dscyrescotti@gmail.com>
2022-08-30 22:25:56 +0630

    feat: add demo project

---
+++ Demo/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Scotti on 8/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
