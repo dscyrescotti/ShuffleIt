9d1f206403b3ccb1dfee9ea38aee863048ef5895
dscyrescotti <dscyrescotti@gmail.com>
2022-09-28 22:34:30 +0630

    feat: change root view of demo app

--- Demo/Demo/App/MainView.swift
+++ Demo/Demo/App/MainView.swift
@@ -3,46 +3,43 @@ import SwiftUI
 struct MainView: View {
     @State private var route: Route? = nil
     var body: some View {
-        CarouselStackDemoView()
-        //
-//        NavigationView {
-//            List {
-//                Section {
-//                    Button(action: {
-//                        route = .shuffleStack
-//                    }) {
-//                        Label("ShuffleStack (Simple)", systemImage: "square.stack.3d.down.forward.fill")
-//                    }
-//                    Button(action: {
-//                        route = .timingShuffleStack
-//                    }) {
-//                        Label("ShuffleStack (with Timer)", systemImage: "deskclock.fill")
-//                    }
-//                    Button(action: {
-//                        route = .carouselStack
-//                    }) {
-//                        Label("CarouselStack", systemImage: "sparkles.rectangle.stack.fill")
-//                    }
-//                } header: {
-//                    Text("ShuffleStack")
-//                }
-//
-//            }
-//            .listStyle(.insetGrouped)
-//            .foregroundColor(.black)
-//            .navigationTitle("ShuffleIt ✌️")
-//            .fullScreenCover(item: $route) { route in
-//                switch route {
-//                case .shuffleStack:
-//                    ShuffleStackDemoView()
-//                case .timingShuffleStack:
-//                    TimingShuffleStackDemoView()
-//                case .carouselStack:
-//                    CarouselStackDemoView()
-//                }
-//            }
-//        }
-         //
+        NavigationView {
+            List {
+                Section {
+                    Button(action: {
+                        route = .shuffleStack
+                    }) {
+                        Label("ShuffleStack (Simple)", systemImage: "square.stack.3d.down.forward.fill")
+                    }
+                    Button(action: {
+                        route = .timingShuffleStack
+                    }) {
+                        Label("ShuffleStack (with Timer)", systemImage: "deskclock.fill")
+                    }
+                    Button(action: {
+                        route = .carouselStack
+                    }) {
+                        Label("CarouselStack", systemImage: "sparkles.rectangle.stack.fill")
+                    }
+                } header: {
+                    Text("ShuffleStack")
+                }
+
+            }
+            .listStyle(.insetGrouped)
+            .foregroundColor(.black)
+            .navigationTitle("ShuffleIt ✌️")
+            .fullScreenCover(item: $route) { route in
+                switch route {
+                case .shuffleStack:
+                    ShuffleStackDemoView()
+                case .timingShuffleStack:
+                    TimingShuffleStackDemoView()
+                case .carouselStack:
+                    CarouselStackDemoView()
+                }
+            }
+        }
     }
 }
 
