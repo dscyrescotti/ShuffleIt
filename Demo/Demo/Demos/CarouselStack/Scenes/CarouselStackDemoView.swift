2b6716b116871f4058e48aa92f977e8420ab1180
dscyrescotti <dscyrescotti@gmail.com>
2022-09-28 22:11:58 +0630

    feat: add demo for carousel stack

--- Demo/Demo/Demos/CarouselStack/Scenes/CarouselStackDemoView.swift
+++ Demo/Demo/Demos/CarouselStack/Scenes/CarouselStackDemoView.swift
@@ -3,32 +3,124 @@ import Combine
 import ShuffleIt
 
 struct CarouselStackDemoView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var sneaker: Sneaker?
+    @State private var isShowItems: Bool = false
+    let carouselPublisher = PassthroughSubject<CarouselDirection, Never>()
+    let timer = Timer.publish(every: 10, tolerance: 0.1, on: .main, in: .default).autoconnect()
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    let columns: [GridItem] = .init(repeating: GridItem(.flexible(), spacing: 20, alignment: .leading), count: 2)
     let sneakers: [Sneaker] = .sneakers()
-    let slideTrigger1 = PassthroughSubject<CarouselDirection, Never>()
-    let slideTrigger2 = PassthroughSubject<CarouselDirection, Never>()
     
     var body: some View {
-        ZStack {
-            HStack(spacing: 0) {
-                Color.blue
-                Color.cyan
+        VStack(alignment: .leading, spacing: 16) {
+            CarouselStack(sneakers, initialIndex: 0) { sneaker in
+                SneakerCard(sneaker: sneaker, translation: 0)
             }
-            VStack {
-                CarouselStack(sneakers, initialIndex: 0) { sneaker in
-                    SneakerCard(sneaker: sneaker, translation: 0)
+            .carouselScale(0.7)
+            .carouselPadding(horizontalSizeClass == .compact ? 20 : 40)
+            .carouselSpacing(horizontalSizeClass == .compact ? 10 : 30)
+            .carouselStyle(.infiniteScroll)
+            .onCarousel { context in
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    isShowItems = false
                 }
-                .carouselScale(0.8)
-                .carouselPadding(30)
-                .carouselSpacing(20)
-                .carouselStyle(.infiniteScroll)
-                CarouselStack(sneakers, initialIndex: 0) { sneaker in
-                    SneakerCard(sneaker: sneaker, translation: 0)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    withAnimation(.easeInOut(duration: 0.2)) {
+                        sneaker = sneakers[context.index]
+                        isShowItems = true
+                    }
                 }
-                .carouselScale(0.5)
-                .carouselPadding(30)
-                .carouselSpacing(20)
-                .carouselStyle(.finiteScroll)
             }
+            .carouselTrigger(on: carouselPublisher)
+            .carouselAnimation(.easeInOut)
+            if let sneaker = sneaker, isShowItems {
+                Text("Explore in \(sneaker.title)")
+                    .font(.title.bold())
+                    .animation(.none)
+                    .transition(AnyTransition.slide.combined(with: .opacity))
+                    .padding(.horizontal, 20)
+            }
+            if let sneaker = sneaker, isShowItems {
+                ScrollView {
+                    Group {
+                        if horizontalSizeClass == .compact {
+                            LazyVStack(alignment: .leading) {
+                                ForEach(sneaker.items) { item in
+                                    SneakerItemRow(item: item)
+                                }
+                            }
+                        } else {
+                            LazyVGrid(columns: columns) {
+                                ForEach(sneaker.items) { item in
+                                    SneakerItemRow(item: item)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .animation(.none, value: sneaker.id)
+                }
+                .transition(AnyTransition.move(edge: .bottom).combined(with: .opacity))
+                .modifier(DragGestureViewModifier(onEnd: { value in
+                    switch(value.translation.width, value.translation.height) {
+                    case (...0, -30...30):
+                        carouselPublisher.send(.right)
+                    case (0..., -30...30):
+                        carouselPublisher.send(.left)
+                    default: break
+                    }
+                }))
+                .padding(.bottom, 20)
+            } else {
+                Spacer()
+            }
+        }
+        .background {
+            if let sneaker = sneaker {
+                GeometryReader { proxy in
+                    ZStack {
+                        Circle()
+                            .scale(1.1, anchor: .bottomTrailing)
+                            .position(x: proxy.size.width, y: proxy.size.height / 2.5)
+                            .foregroundColor(Color(hex: sneaker.theme.tertiary).opacity(0.3))
+                        Circle()
+                            .scale(0.7, anchor: .bottomTrailing)
+                            .position(x: 0, y: proxy.size.height)
+                            .foregroundColor(Color(hex: sneaker.theme.primary).opacity(0.45))
+                    }
+                    .blur(radius: 40)
+                    .background(.white)
+                    .ignoresSafeArea()
+                }
+            }
+        }
+        .onAppear {
+            sneaker = sneakers.first
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    isShowItems = true
+                }
+            }
+        }
+        .overlay(alignment: .topTrailing) {
+            Button(action: {
+                dismiss()
+            }) {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .background {
+                        Circle()
+                            .foregroundColor(.black.opacity(0.4))
+                            .padding(4)
+                    }
+            }
+            .buttonStyle(.plain)
+            .padding(.horizontal, 20)
+        }
+        .onReceive(timer) { _ in
+            carouselPublisher.send(.right)
         }
     }
 }
