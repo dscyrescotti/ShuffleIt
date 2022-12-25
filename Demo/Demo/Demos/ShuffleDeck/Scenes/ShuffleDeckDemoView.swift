ea3631aa6829a0ee935b348d49bdaf9b1338d03c
dscyrescotti <dscyrescotti@gmail.com>
2022-12-25 22:26:35 +0630

    feat: revamp demo ui

--- Demo/Demo/Demos/ShuffleDeck/Scenes/ShuffleDeckDemoView.swift
+++ Demo/Demo/Demos/ShuffleDeck/Scenes/ShuffleDeckDemoView.swift
@@ -5,7 +5,6 @@ import MapKit
 
 struct ShuffleDeckDemoView: View {
     @Environment(\.dismiss) var dismiss
-    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09))
     @State private var landmark: Landmark?
 
     let landmarks: [Landmark] = .landmarks()
@@ -19,6 +18,7 @@ struct ShuffleDeckDemoView: View {
                     .font(.title2.bold())
             }
             .padding()
+            Spacer()
             ShuffleDeck(
                 landmarks,
                 initialIndex: 0
@@ -28,22 +28,11 @@ struct ShuffleDeckDemoView: View {
             .shuffleDeckAnimation(.easeInOut)
             .shuffleDeckScale(0.5)
             .onShuffleDeck { context in
-                let landmark = landmarks[context.index]
-                self.landmark = landmark
-                withAnimation(.easeInOut) {
-                    region.center = landmark.coordinate
-                }
+                landmark = landmarks[context.index]
             }
             .padding(.vertical)
-            VStack(alignment: .leading) {
-                Text("View on Map")
-                    .font(.headline.bold())
-                Map(coordinateRegion: $region, annotationItems: landmarks) { landmark in
-                    MapPin(coordinate: landmark.coordinate)
-                }
-                .cornerRadius(15)
-            }
-            .padding()
+            Spacer()
+            Spacer()
         }
         .overlay(alignment: .topTrailing) {
             Button(action: {
@@ -72,7 +61,7 @@ struct ShuffleDeckDemoView: View {
                         Circle()
                             .scale(0.7, anchor: .bottomTrailing)
                             .position(x: 0, y: proxy.size.height / 1.5)
-                            .foregroundColor(Color(hex: landmark.background).opacity(0.45))
+                            .foregroundColor(Color(hex: landmark.background).opacity(0.2))
                     }
                     .blur(radius: 40)
                     .background(.white)
@@ -81,11 +70,7 @@ struct ShuffleDeckDemoView: View {
             }
         }
         .onAppear {
-            guard let landmark = landmarks.first else { return }
-            self.landmark = landmark
-            withAnimation(.easeInOut) {
-                region.center = landmark.coordinate
-            }
+            landmark = landmarks.first
         }
     }
 }
